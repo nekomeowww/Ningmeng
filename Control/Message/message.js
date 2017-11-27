4151418cf79876d6fa090f23ace9b27fec35d25f
Ayaka Neko <neko@ayaka.moe>
2017-11-27 15:55:17 +0800

    Advanced replies added

--- Control/Message/message.js
+++ Control/Message/message.js
@@ -3,6 +3,7 @@
 // Import
 
 let botctl = require('../control');
+let nlpctl = require('./nlp/nlp');
 
 // Message Control
 
@@ -19,11 +20,19 @@ let msgctl = {
 
     // Define Recieve Actions
 
-    botctl.message.hears('hi');
+    botctl.message.hears(/(?:hi)|(?:你好)/gi);
 
     // Define Reply Actions
+    
+    let lovefeather = /(?:喜欢羽毛)|(?:爱羽毛)/g;
+    let loveneko = /(?:喜欢 Neko)|(?:喜欢Neko)/gi;
+    let talk = /(?:柠檬)((?!.))|(?:柠檬酱)((?!.))/g;
+    let sadmeow = /(?:喵)(?:\.)+/g;
 
-    botctl.message.hearsRpy('喜欢羽毛 /', '好耶 /');
+    botctl.message.hearsRpy(lovefeather, '好耶，Neko 说她也超喜欢羽毛的w');
+    botctl.message.hearsRpy(loveneko, "Neko 知道的话会超开心的！")
+    botctl.message.hearsRpy(talk, "柠檬酱在的，有什么事情吗？");
+    botctl.message.hearsRpy(sadmeow, "唔...揉揉");
 
     // Define Sticker Actions
 
@@ -31,7 +40,11 @@ let msgctl = {
     // Each sticker the bot recieved will cause the bot do this action
     //botctl.message.sticker();
     
+
+    
     }
 }
+
+botctl.Bot.on
 
 module.exports = msgctl;
---
+++ Control/Message/nlp/nlp.js
@@ -0,0 +1,5 @@
+let nlp = {
+    
+}
+
+module.exports = nlp;
