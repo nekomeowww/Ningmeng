97ba3b2de98e22714af3dabcf8f949476c5a3231
Enfu Zhang <fanff0324@gmail.com>
2017-12-12 15:29:24 +0800

    Ordering with Zeta

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
-// Webhook
-
-const Koa = require('koa');
-const koaBody = require('koa-body');
-
 // Local Files
 
 let bot = require('./bot');
 let config = require('./config');
 
-let webhookUrl = config.webhook.url;
-let webhookPath = config.webhook.path;
-let webhookPort = config.webhook.port;
-
-const app = new Koa();
-
 bot.botctl.start();
-
-bot.Bot.telegram.setWebhook(webhookUrl + webhookPath);
-
-app.use(koaBody());
-app.use((ctx, next) => ctx.method === 'POST' || ctx.url === webhookPath
-    ? bot.Bot.handleUpdate(ctx.request.body, ctx.response)
-    : next()
-);
-
 bot.Log.info("柠檬现在已经在线上啦！");
-
-app.listen(webhookPort);
+bot.Bot.startPolling();
