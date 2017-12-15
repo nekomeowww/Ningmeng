625a437d55df7334eb974118e6af67ec7e7cf05f
Enfu Zhang <fanff0324@gmail.com>
2017-12-15 21:37:24 +0800

    Impliment with text, sticker, photo processing

--- control.js
+++ control.js
@@ -1,44 +1,65 @@
 // Local Files
 
-let botctl = require('./bot');
+let bot = require('./bot');
 let config = require('./config');
+let msgctl = require('./Bot/message');
+let stkctl = require('./Bot/sticker');
 let packageInfo = require('./package.json');
-
-let botUsername = "@" + config.username;
-
 
 let core = {
     control: () => {
         // Command
-        botctl.Bot.command(command.register('/help'), (ctx) => ctx.reply("随意说话就好了喵w"));
-        botctl.Bot.command(command.register('/commandCheck'), (ctx) => ctx.reply(command.commandCheck(ctx.message.text)));
-        botctl.Bot.command(command.register('/info'), (ctx) => info.info(ctx));
-        botctl.Bot.hears('喵', (ctx) => ctx.reply('喵'));
+
+        bot.Bot.command(command.register('/help'), (ctx) => ctx.reply("随意说话就好了喵w"));
+        bot.Bot.command(command.register('/commandCheck'), (ctx) => ctx.reply(command.commandCheck(ctx.message.text)));
+        bot.Bot.command(command.register('/info'), (ctx) => info.info(ctx));
+        bot.Bot.hears('喵', (ctx) => ctx.reply('喵'));
+
+        // Context Processing
+        
+        // Text Handling
+        bot.Bot.on('text', (ctx) => {
+            bot.Log.trace(ctx.message.text);
+            msgctl.core(ctx);
+            return ctx.message.text
+        })
+        // Sticker Handling
+        bot.Bot.on('sticker', (ctx) => {
+            stkctl.core(ctx);
+            return ctx.reply('喵 /');
+        });
+        // Photo Handling
+        bot.Bot.on('photo', (ctx) => {
+            //bot.Log.debug(ctx.message);
+            return ctx.reply('喵 /');
+        });
+        //Channel Post Handling
+        bot.Bot.on('channel_post', (ctx) => {
+
+        });
     }
 }
 
 let command = {
     register: (commandName) => {
-        commands = [commandName, commandName + botUsername];
-        botctl.Log.debug(commands);
+        commands = [commandName, commandName + bot.botUsername];
+        bot.Log.debug(commands);
         return commands;
     },
 
     commandCheck: (text) => {
         let result = "";
         
-        botctl.Log.debug("Data before slice: " + text);
+        bot.Log.debug("Data before slice: " + text);
 
         if(/@NingmengBot/gi.test(text)) {
             result = text.slice(26);
-            botctl.Log.debug("Data after slice: " + result);
+            bot.Log.debug("Data after slice: " + result);
         }
         else {
             result = text.slice(14);
-            botctl.Log.debug("Data after slice: " + result);
+            bot.Log.debug("Data after slice: " + result);
         }
-
-
     }
 }
 
@@ -57,21 +78,21 @@ let info = {
 
         ctx.reply(Version);
 
-        botctl.Log.trace(">>> INFO -" + ctx.message.date + "- Report");
-        botctl.Log.trace(messageId);
-        botctl.Log.trace(messageType);
-        botctl.Log.trace(senderId);
+        bot.Log.trace(">>> INFO -" + ctx.message.date + "- Report");
+        bot.Log.trace(messageId);
+        bot.Log.trace(messageType);
+        bot.Log.trace(senderId);
 
         if(ctx.message.chat.type == 'private') {
             ctx.reply(happyChat);
-            botctl.Log.trace(">>> INFO Report END <<<");
+            bot.Log.trace(">>> INFO Report END <<<");
         }
         else {
             ctx.reply(happyGroup);
 
-            botctl.Log.trace(chatTitle);
-            botctl.Log.trace(chatId);
-            botctl.Log.trace(">>> INFO Report END <<<");
+            bot.Log.trace(chatTitle);
+            bot.Log.trace(chatId);
+            bot.Log.trace(">>> INFO Report END <<<");
         }
     }
 }
