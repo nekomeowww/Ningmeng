d9d3dea0505f8a176baa266cf9ba66d3b02c0267
Ayaka Neko <neko@ayaka.moe>
2017-12-18 17:33:58 +0800

    Fixing the problem with flight track not being returned correctly.

--- Plugins/flightTrack.js
+++ Plugins/flightTrack.js
@@ -35,9 +35,7 @@ let tracker = {
     let flightNum = new String(""); 
     let flightData = flightNumPattern[Symbol.match](data);
     if(!flightData || flightData == null) {
-      ctx.reply("柠檬不知道你的航班号是什么了啦");
-      ctx.reply("使用方法： /flight AR-NUMB YYYY-MM-DD");
-      ctx.reply("AR 是航空公司短标识，NUMB 是航线标识，日期格式应为：1970-01-01");
+      ctx.reply("柠檬不知道你的航班号是什么了啦~ \n使用方法： /flight AR-NUMB YYYY-MM-DD \nAR 是航空公司短标识，NUMB 是航线标识，日期格式应为：1970-01-01");
       return;
     }
 
@@ -50,6 +48,11 @@ let tracker = {
     if(!date || date == null) {
       date = CurrentTime;
     }
+
+    let yearDate = +CurrentTime.substr(0, 4),     // get the year
+        monthDate = +CurrentTime.substr(5, 2) - 1, // get the month
+        dayDate = +CurrentTime.substr(8, 2),     // get the date of the month
+        newDateInfo = new Date(yearDate, monthDate, dayDate);
 
     let flight;
     let botlog = bot.Log;
@@ -77,14 +80,16 @@ let tracker = {
     }
 
     let info = [flight, date]
-    this.core(ctx, info, data);
+    let result = this.core(ctx, info, data);
   },
   core(ctx, info, flight) {
-    bot.Log.debug("用户 " + ctx.message.from.id + " 申请查询航班信息: " + info);
-    ctx.reply("正在申请查询航班信息: " + info[0] + " " + info[1]);
+    bot.Log.debug("用户 " + ctx.message.from.id + " 申请查询航班信息: " + info[0] + "" + info[1]);
+    ctx.reply("正在申请查询航班信息: " + info[0]);
+
     // Link Prefix
 
     let linkPrefix;
+
     switch(config.plugins.flightTrack.locale) {
       case "zh-CHS":
         linkPrefix = "https://www.cn.kayak.com/tracker/";
@@ -101,35 +106,54 @@ let tracker = {
     }
 
     let link = linkPrefix + info[0] + "/" + info[1];
-
     bot.Log.debug(link);
 
-    let status;
-    
     requestPromise(link)
     .then(function (htmlString) {
       // Process html...
-      status = newArray(parse(htmlString));
+      
+      var $ = cheerio.load(htmlString);
+      var flightStatus = $('div.statusLines').text().split('\n');
 
-      bot.Log.debug(status);
+      // Reply to user
 
-      for(var i; i < status.length; i++) {
-        ctx.reply(status[i]);
+      let flight = {
+        depart : flightStatus[1],
+        departAirport : flightStatus[2],
+        departSchechuleDate : flightStatus[3],
+        departSchechuleDateInfo : flightStatus[4],
+        departSchechuleTime : flightStatus[5],
+        departSchechuleTimeInfo : flightStatus[6],
+        departActualTime : flightStatus[7],
+        departActualTimeInfo : flightStatus[8],
+        departTerminal : flightStatus[9],
+        departTerminalInfo : flightStatus[10],
+        departGate : flightStatus[11],
+        departGateInfo : flightStatus[12],
+
+        arrival : flightStatus[14],
+        arrivalAirport : flightStatus[15],
+        arrivalSchechuleDate : flightStatus[16],
+        arrivalSchechuleDateInfo : flightStatus[17],
+        arrivalSchechuleTime : flightStatus[18],
+        arrivalSchechuleTimeInfo : flightStatus[19],
+        arrivalActualTime : flightStatus[20],
+        arrivalActualTimeInfo : flightStatus[21],
+        arrivalTerminal : flightStatus[22],
+        arrivalTerminalInfo : flightStatus[23],
+        arrivalGate : flightStatus[24],
+        arrivalGateInfo : flightStatus[25],
       }
 
-      return status;
+      ctx.reply(flight.departAirport + "->" + flight.arrivalAirport + "\n" + flight.depart + "\n" + flight.departSchechuleDate + ": " + flight.departSchechuleDateInfo + "\n" + flight.departSchechuleTime + ": " + flight.departSchechuleTimeInfo + "\n" + flight.departActualTime + ": " + flight.departActualTimeInfo + "\n" + flight.departTerminal + ": " + flight.departTerminalInfo + " " + flight.departGate + ": " + flight.departGateInfo + "\n" +
+                                                                            flight.arrival + "\n" + flight.arrivalSchechuleDate + ": " + flight.arrivalSchechuleDateInfo + "\n" + flight.arrivalSchechuleTime + ": " + flight.arrivalSchechuleTimeInfo + "\n" + flight.arrivalActualTime + ": " + flight.arrivalActualTimeInfo + "\n" + flight.arrivalTerminal + ": " + flight.arrivalTerminalInfo + " " + flight.arrivalGate + ": " + flight.arrivalGateInfo);
+
     })
     .catch(function (err) {
       // Crawling failed...
+      bot.Log.fatal("用户 " + ctx.message.from.id + " 查询的航班信息获取失败。");
       ctx.reply("抱歉，航班查询服务目前暂不可用。");
     });
-
-    
-  },
-  parse(body) {
-      var $ = cheerio.load(body);
-      var result = $('div.statusLines').text().split('\n')
-      return result;
   }
 }
 
