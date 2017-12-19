0b95d2cf091ef6fa14304f7497f366a43c230e82
Ayaka Neko <neko@ayaka.moe>
2017-12-19 09:13:04 +0800

    Clean up in control.js and implimented the command management

--- control.js
+++ control.js
@@ -4,23 +4,32 @@ let bot = require('./bot');
 let config = require('./config');
 let msgctl = require('./Bot/message');
 let stkctl = require('./Bot/sticker');
+let cmdctl = require('./Bot/command');
 let plugin = require('./Plugins/plugin');
 let packageInfo = require('./package.json');
 
 let core = {
+
+    // Main control of all things that requires ctx
+
     control: () => {
+
         // Command
 
         bot.Bot.command(command.register('/start'), (ctx) => ctx.reply("你好喔，这里是柠檬酱。想要开始的话直接选择命令或者直接说话就好了喔w"));
         bot.Bot.command(command.register('/help'), (ctx) => ctx.reply("随意说话就好了喵w"));
-        bot.Bot.command(command.register('/info'), (ctx) => info.info(ctx));
-        bot.Bot.command(command.register('/progynova'), (ctx) => ctx.reply("是黄色的糖糖! "));
-        bot.Bot.command(command.register('/pat'), (ctx) => ctx.reply("（呼噜呼噜声"));
+        bot.Bot.command(command.register('/info'), (ctx) => cmdctl.info(ctx));
         bot.Bot.command(command.register('/flight'), (ctx) => plugin.track(ctx));
+        bot.Bot.command(command.register('/nlp'), (ctx) => msgctl.nlpProcessor.command(ctx));
+        bot.Bot.command(command.register('/progynova'), (ctx) => ctx.reply("是甜甜的糖糖! "));
+        bot.Bot.command(command.register('/androcur'), (ctx) => ctx.reply("是白色的糖糖！"));
+        bot.Bot.command(command.register('/estrofem'), (ctx) => ctx.reply("是蓝色的糖糖！"));
+        bot.Bot.command(command.register('/pat'), (ctx) => ctx.reply("（呼噜呼噜声"));
 
         // Context Processing
         
         // Text Handling
+
         let botlog = bot.Log;
         let msgcore = msgctl.msgctl;
 
@@ -38,18 +47,25 @@ let core = {
             }
 
             // Port to processor
+
             msgcore.core(ctx);
             return ctx.message.text;
         })
+
         // Sticker Handling
+
         bot.Bot.on('sticker', (ctx) => {
             stkctl.core(ctx);
         });
+
         // Photo Handling
+
         bot.Bot.on('photo', (ctx) => {
             //bot.Log.debug(ctx.message);
         });
+
         //Channel Post Handling
+
         bot.Bot.on('channel_post', (ctx) => {
 
         });
@@ -57,10 +73,15 @@ let core = {
 }
 
 let command = {
+
+    // Register the command and also checkin command with bot's ssername
+
     register: (commandName) => {
         commands = [commandName, commandName + bot.botUsername];
         return commands;
     },
+
+    // Return the pure data without the "/command" part of string
 
     commandCheck: (ctx) => {
         let result = "";
@@ -74,40 +95,6 @@ let command = {
         else {
             result = text.slice(split + 1);
             return result;
-        }
-    }
-}
-
-let info = {
-    info: (ctx) => {
-        let Version = bot.botUsername + " 版本: " + packageInfo.version;
-        let messageId = "Message ID: " + ctx.message.message_id;
-        let messageType = "Message Type: " + ctx.message.chat.type;
-        let senderId = "Sender ID: " + ctx.message.from.id;
-
-        let chatTitle = "Chat Title: " + ctx.message.chat.title;
-        let chatId = "Chat ID: " +  ctx.message.chat.id;
-
-        let happyChat = "希望你开心喔！";
-        let happyGroup = "希望你喜欢" + config.nickname + "喔！";
-
-        ctx.reply(Version);
-
-        bot.Log.trace(">>> INFO -" + ctx.message.date + "- Report");
-        bot.Log.trace(messageId);
-        bot.Log.trace(messageType);
-        bot.Log.trace(senderId);
-
-        if(ctx.message.chat.type == 'private') {
-            ctx.reply(happyChat);
-            bot.Log.trace(">>> INFO Report END <<<");
-        }
-        else {
-            ctx.reply(happyGroup);
-
-            bot.Log.trace(chatTitle);
-            bot.Log.trace(chatId);
-            bot.Log.trace(">>> INFO Report END <<<");
         }
     }
 }
